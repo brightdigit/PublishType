17453e5b5c3836cd814c266a49844c1257b42eb0
Leo Dion <leogdion@brightdigit.com>
2026-07-28 07:48:16 -0400

    v1.0.0-alpha.1

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version:6.4
+// swiftlint:disable explicit_acl explicit_top_level_acl
+
+import PackageDescription
+
+let package = Package(
+  name: "PublishType",
+  // Files (via Publish) needs iOS 18 / tvOS 18 / watchOS 11 for Synchronization.Mutex.
+  platforms: [
+    .macOS(.v15),
+    .iOS(.v18),
+    .tvOS(.v18),
+    .watchOS(.v11)
+  ],
+  products: [
+    .library(
+      name: "PublishType",
+      targets: ["PublishType"]
+    )
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/brightdigit/Publish.git",
+      from: "1.0.0-alpha.1"
+    )
+  ],
+  targets: [
+    .target(
+      name: "PublishType",
+      dependencies: [
+        .product(name: "Publish", package: "Publish")
+      ]
+    ),
+    .testTarget(
+      name: "PublishTypeTests",
+      dependencies: ["PublishType"]
+    )
+  ]
+)
